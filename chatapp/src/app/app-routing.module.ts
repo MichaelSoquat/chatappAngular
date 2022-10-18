5ffc14616cda35e0710064ce403245555cc6749f
MichaelSoquat <michael.soquat@gmx.de>
2022-10-18 07:55:06 +0200

    added structure

--- chatapp/src/app/app-routing.module.ts
+++ chatapp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./menus/menus.module').then(m => m.MenusModule) },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
