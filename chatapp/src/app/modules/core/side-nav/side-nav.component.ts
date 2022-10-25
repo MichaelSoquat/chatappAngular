90eed06ff3fc9b011c03e58d5f3709162123ffb5
MichaelSoquat <michael.soquat@gmx.de>
2022-10-25 09:16:21 +0200

    added channel create field

--- chatapp/src/app/modules/core/side-nav/side-nav.component.ts
+++ chatapp/src/app/modules/core/side-nav/side-nav.component.ts
@@ -7,7 +7,7 @@ import { Observable } from 'rxjs';
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss'],
-  
+  encapsulation: ViewEncapsulation.None,
 })
 export class SideNavComponent implements OnInit {
   @Input() sidenavStatus$: Observable<boolean> = new Observable<boolean>();
